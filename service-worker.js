a5026ba4ff902984ef5c28f521e27fcb5a3352a5
calehunkin <cale.hunkin@gmail.com>
2026-07-02 13:06:24 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,19 @@
+const CACHE_NAME = "driving-app-v4";
 
-const CACHE_NAME = 'driving-app-v2';
-const urlsToCache = ['./', './index.html'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", event => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.map(key => caches.delete(key)))
+  ));
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
   );
 });
