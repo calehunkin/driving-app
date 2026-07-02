c6199d390b1d6533ae608da2b67ff68ad27ff088
calehunkin <cale.hunkin@gmail.com>
2026-07-02 12:50:06 +1000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+
+const CACHE_NAME = 'driving-app-v2';
+const urlsToCache = ['./', './index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
